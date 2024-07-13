ba3d01415e6f4598ad010f1c1acc07164903a2e7
Shivam Bhardwaj <151818491+Shivam14985@users.noreply.github.com>
2024-07-13 19:40:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Clicking on resume
+
+function resume()
+{
+    window.location.href = "https://firebasestorage.googleapis.com/v0/b/shivam-s-cart-c0e04.appspot.com/o/website%2FResume.png?alt=media&token=eced987a-d818-4e1f-8a40-fd4c805d4cf4";
+}
+
+//view Project1
+function cartproject(){
+    window.location.href = "https://drive.google.com/drive/folders/1oZyhBmzwguDnMe8kpAiHGO5fSKTO-WU6";
+}
+
+//viewPRoject2
+function instagramapp(){
+    window.location.href = "https://drive.google.com/drive/folders/1skEHJBdo5u5lx1T5BsAazTFF0vab_6H7";
+}
+
+//viewPRoject3
+function youtube(){
+    window.location.href="https://drive.google.com/drive/folders/1oaddC4CTr8IcrOjOLR24TXi5dMGkGE_j";
+}
+
+//viewProject4
+function chaties(){
+    window.location.href="https://drive.google.com/drive/folders/1waT9xnMZzY3rLflhRTAYiNkdgGQrh-Dy";
+}
+
+//download project3
